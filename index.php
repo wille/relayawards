c4558fb42d0a9958cc03394b58b8e98f0bb9c218
redpois0n <red@cmail.nu>
2016-03-23 17:17:40 +0100

    FP and adr

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
             <table>
                   <tr>
                         <th>Nickname</th>
+                        <th>Fingerprint</th>
+                        <th>Address</th>
                   </tr>
 
 <?php
@@ -14,7 +16,11 @@ require_once "relays.php";
 $relays = Relays::query_relays(htmlspecialchars($_GET["s"]));
 
 foreach ($relays as $relay) {
-      echo "<tr><td>" . $relay->nick . "</td></tr>";
+      echo "<tr>";
+      echo "<td>" . $relay->nick . "</td>";
+      echo "<td>" . $relay->fingerprint . "</td>";
+      echo "<td>" . $relay->or_addresses[0] . "</td>";
+      echo "</tr>";
 }
 
 ?>
