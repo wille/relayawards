7e335f0da829c9d9d3e274e9e80d2c258633537e
intchloe <chloe@cmail.nu>
2016-04-11 01:55:51 +0200

    Update exit_probability_2.php

--- awards/exit_probability_2.php
+++ awards/exit_probability_2.php
@@ -7,15 +7,15 @@ class ExitProbability2 extends Award {
       }
 
       public function get_icon() {
-            return "exit_prob_0.5%.png";
+            return "exit_prob_2.png";
       }
 
       public function get_description() {
-            return "Over 0.05% mean exit probability fraction";
+            return "Over 0.5% mean exit probability fraction";
       }
 
       public function is_granted($relay) {
-            return $relay->exit_probability >= 0.05;
+            return $relay->exit_probability >= 0.5;
       }
 
 }
