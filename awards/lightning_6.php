a76f2fd066fa606513110c967e92cd1cbc81e018
intchloe <chloe@cmail.nu>
2016-04-11 01:43:02 +0200

    Create lightning_6.php

---
+++ awards/lightning_6.php
@@ -0,0 +1,18 @@
+<?php
+class Lightning50 extends Award {
+      public function get_name() {
+            return "Lightning 5";
+      }
+      public function get_icon() {
+            return "king.png";
+      }
+      public function get_description() {
+            return "Over 75 MB/s in bandwidth. You are the king!";
+      }
+      public function is_granted($relay) {
+            $mb = pow(1024, 2) * 75;
+            
+            return $relay->bandwidth >= $mb;
+      }
+}
+?>
