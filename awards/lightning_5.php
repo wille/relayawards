2dccb59f76366cedac83aaa9a7b3c27464e9e7b5
redpois0n <red@cmail.nu>
2016-04-10 22:33:57 +0200

    Format

--- awards/lightning_5.php
+++ awards/lightning_5.php
@@ -1,16 +1,21 @@
 <?php
 class Lightning50 extends Award {
+
       public function get_name() {
             return "Lightning 5";
       }
+
       public function get_icon() {
             return "50_mb_speed.png";
       }
+
       public function get_description() {
             return "Over 50 MB/s in bandwidth";
       }
+
       public function is_granted($relay) {
             $mb = pow(1024, 2) * 50;
+            
             return $relay->bandwidth >= $mb;
       }
 }
