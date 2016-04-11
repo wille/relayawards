cdf9b50acee7ca38249d00106fca518bafe415f9
redpois0n <red@cmail.nu>
2016-04-11 08:45:53 +0200

    Format & add more awards

--- awards/exit_probability_4.php
+++ awards/exit_probability_4.php
@@ -1,14 +1,18 @@
 <?php
 class ExitProbability4 extends Award {
+
       public function get_name() {
             return "Exit Probability 4";
       }
+
       public function get_icon() {
             return "king.png";
       }
+
       public function get_description() {
             return "Over 2% mean exit probability fraction";
       }
+
       public function is_granted($relay) {
             return $relay->exit_probability >= 2;
       }
--- awards/middle_probability_3.php
+++ awards/middle_probability_3.php
@@ -1,14 +1,18 @@
 <?php
 class MiddleProbability3 extends Award {
+
       public function get_name() {
             return "Middle Probability 3";
       }
+
       public function get_icon() {
             return "middle_prob_3.png";
       }
+
       public function get_description() {
             return "Over 1% mean middle probability fraction";
       }
+      
       public function is_granted($relay) {
             return $relay->middle_probability >= 1;
       }
