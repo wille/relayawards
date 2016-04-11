7e0461eb05fa3207ac353943a39f605a41ecdba4
intchloe <chloe@cmail.nu>
2016-04-11 03:05:28 +0200

    Update awards.php

--- awards/awards.php
+++ awards/awards.php
@@ -12,7 +12,11 @@ abstract class Award {
       public abstract function is_granted($relay);
 
 }
-
+include_once "unix.php";
+include_once "guard.php";
+include_once "alpha.php";
+include_once "https_lover.php";
+include_once "exotic.php";
 
 include_once "blue_diamond.php";
 include_once "red_diamond.php";
@@ -21,22 +25,22 @@ include_once "lightning_1.php";
 include_once "lightning_2.php";
 include_once "lightning_3.php";
 include_once "lightning_4.php";
-
-include_once "https_lover.php";
-include_once "exotic.php";
+include_once "lightning_5.php";
+include_once "lightning_6.php";
+include_once "lightning_7.php";
 
 include_once "consensus_weight_1.php";
 include_once "consensus_weight_2.php";
 
 include_once "exit_probability_1.php";
 include_once "exit_probability_2.php";
+include_once "exit_probability_3.php";
+include_once "exit_probability_4.php";
 
 include_once "middle_probability_1.php";
 include_once "middle_probability_2.php";
-
-include_once "unix.php";
-include_once "guard.php";
-include_once "alpha.php";
+include_once "middle_probability_3.php";
+include_once "middle_probability_4.php";
 
 $awards = [
       new Red_Diamond(), new Blue_Diamond(),
