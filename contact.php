977b9f07fe859af99ed5d018137b43fccc410013
redpois0n <red@cmail.nu>
2016-04-13 07:31:09 +0200

    Update header and contact.php

--- contact.php
+++ contact.php
@@ -1,50 +1,6 @@
-<html>
-
-<head>
-      <link rel="shortcut icon" href="images/static/favicon.ico" />
-      <title>RelayAwards - Contact</title>
-      <link href="css/bootstrap.min.css" rel="stylesheet">
-      <link href='css/roboto.css' rel='stylesheet' type='text/css'>
-	<link href="css/font-awesome.min.css" rel="stylesheet">
-      <style>
-            body {
-                  padding-top: 70px;
-		  font-family: 'Roboto', sans-serif;
-      }
-</style>
-</head>
-
-      <body>
-
-          <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-              <div class="container">
-                  <div class="navbar-header">
-                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                          <span class="sr-only">Toggle navigation</span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                          <span class="icon-bar"></span>
-                      </button>
-      		<a class="navbar-brand" href="/">RelayAwards</a>
-                  </div>
-
-                  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                      <ul class="nav navbar-nav">
-                          <li>
-                              <a href="awards.php">Awards</a>
-                          </li>
-	                   <li>
-                              <a href="info.php">Information</a>
-                          </li>
-
-			<li>
-                             <a href="contact.php"> Contact </a>
-                          </li>
-
-                      </ul>
-                  </div>
-              </div>
-          </nav>
+<?php
+require_once "header.php";
+?>
 <center><h2>Contact</h2></br>
 <img src="/images/static/chloe.png">
 </br></br>
