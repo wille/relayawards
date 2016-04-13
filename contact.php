ae707eb995193e45ee32f44decc6d7a30a99bb3a
redpois0n <red@cmail.nu>
2016-04-13 07:36:54 +0200

    Fix path to icon

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 require_once "header.php";
 ?>
 <center><h2>Contact</h2></br>
-<img src="/images/static/chloe.png">
+<img src="images/static/chloe.png">
 </br></br>
 <i class="fa fa-envelope" aria-hidden="true"></i> chloe@chloe.re </br>
 <i class="fa fa-twitter" aria-hidden="true"></i> <a href="https://twitter.com/dotchloe"> @dotchloe </a></br>
