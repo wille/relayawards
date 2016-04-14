684faadebf694fd10b9588c89de3af972c408643
intchloe <chloe@cmail.nu>
2016-04-14 22:58:02 +0200

    Added much info to about.php

--- about.php
+++ about.php
@@ -2,12 +2,44 @@
 require_once "header.php";
 ?>
 
-<center><h2>Information about RelayAwards</h2></br>
-<h4>Rules</h4>
-The rules are pretty simple: there's a <a href="awards.php">list of awards</a> that each relay will earn if it reaches the requirements.<p>
-Some awards are easy to earn because they only require the operator to follow best practices.
-Some awards will take time and a lot of effort to earn.
-There are even awards that are almost impossible to reach and if actually do reach them you will earn the glorious crowne!<p>
+<center><h1>Information about RelayAwards</h1></br>
+<h4>What is RelayAwards?</h4>
+RelayAwards is a competition for all active and running Tor-relays. We believe that this can motivate operators to develop and reach new heights as a relay operator and therefore help the Tor network.<p>
+You can compete against your friends, your family or why not yourself?! You can even try to become the best relay in your country! </br>
+We built RelayAwards because we wanted something more than <a href="https://globe.torproject.org/">Globe</a>, something that motivated us to grow and become better as relay operators. We think it should be fun to do a good cause!<p>
+
 </br></br>
+<h4>Rules</h4>
+The rules are pretty simple: there's a <a href="/awards.php">list of awards</a> that each relay will earn if it reaches the requirements.</br>
+Some awards are easy to earn because they only require the operator to follow best practices.</br>
+Some awards will take time and a lot of effort to earn.
+There are even awards that are almost impossible to reach and if actually do reach them you will earn the glorious <a href="/images/rewards/king.png">crowne!</a><p>
+</br></br>
+<h4>FAQ</h4>
+<b>Q: How do I participate?</b></br>
+As long as you have a public and running relay you are participating! This is a competition for all public Tor-relays. </br></br>
+
+<b>Q: Why does running an Alpha-version give 1 point?</b></br>
+Running Alpha is not really best practices but we do believe running running an Alpha-version may help finding bugs.</br></br>
+
+<b>Q: Some of my awards went away! Why?</b></br>
+Relays will only earn awards if they are running and are currently reaching the requirements for the award so your relay either rebooted or does not qualify for the award. </br></br>
+
+<b>Q: What is an exotic country?</b></br>
+An exotic country is a country where few relays have been active historically. Data grabbed from <a href="https://torflow.uncharted.software/">Torflow.</a>
+You can find the 2-letter ISO codes for them in the <a href="https://github.com/redpois0n/relayawards/blob/master/awards/exotic.php#L19">code.</a></br></br>
+
+<b>Q: Will there be more awards in the future?</b></br>
+Yes, hopefully! You can help us adding awards by creating an issue at <a href="https://github.com/redpois0n/relayawards/issues">Github.</a>
+
+</br></br></br>
 <h4>About the code</h4>
-RelayAwards uses the onionoo API for fetching statistics from the Tor-network. The code is written in PHP and uses bootstrap as template.
+RelayAwards uses the <a href="https://onionoo.torproject.org/">onionoo API</a> for fetching statistics from the Tor-network. The code is written in PHP and uses bootstrap as template.
+</br>RelayAwards is open source and licensed under. Code can be found at <a href="https://github.com/redpois0n/relayawards">Github</a>.
+</br><div>Icons made by <a href="http://www.flaticon.com/authors/vector-stall" title="Vector Stall">Vector Stall</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+</br></br></br>
+<h4>About the site</h4>
+Relayawards.com does <b><u>NOT</b></u>: Store any information about its visitors, require Javascript or use any other tracking methods such as cookies, third-party content or etags.<p>
+Relayawards.com uses preload-HSTS, CSP and other <a href="https://securityheaders.io/?q=https%3A%2F%2Frelayawards.com">security headers</a>. Certificate is from <a href="https://letsencrypt.org/">Let's Encrypt</a>, see also <a href="https://www.ssllabs.com/ssltest/analyze.html?d=relayawards.com&latest">SSLlabs</a>.
+</br></br></br></br>
+
