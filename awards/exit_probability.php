f6cc3bf22367ca74a2f24769b3a440e4ddca488f
redpois0n <red@cmail.nu>
2016-04-14 22:19:47 +0200

    Exit prob sorted (0.01 missing)

---
+++ awards/exit_probability.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once "abstract_level.php";
+
+abstract class ExitProbability extends AbstractLevelAward {
+
+      public function get_name() {
+            return "Exit Probability " . $this->level;
+      }
+
+      public function get_icon() {
+            return $king ? "king.png" : "exit_prob_" . $this->level . ".png";
+      }
+
+      public function get_description() {
+            return "Over " . $this->fraction . "% mean exit probability fraction";
+      }
+
+      public function is_granted($relay) {
+            return $relay->exit_probability >= $this->fraction;
+      }
+}
+
+class ExitProbability1 extends ExitProbability {
+
+      public function __construct() {
+            parent::__construct(1, 0.001);
+      }
+}
+
+class ExitProbability2 extends ExitProbability {
+
+      public function __construct() {
+            parent::__construct(2, 0.01);
+      }
+}
+
+class ExitProbability3 extends ExitProbability {
+
+      public function __construct() {
+            parent::__construct(3, 0.1);
+      }
+}
+
+class ExitProbability4 extends ExitProbability {
+
+      public function __construct() {
+            parent::__construct(4, 0.5);
+      }
+}
+
+class ExitProbability5 extends ExitProbability {
+
+      public function __construct() {
+            parent::__construct(5, 1);
+      }
+}
+
+class ExitProbability6 extends ExitProbability {
+
+      public function __construct() {
+            parent::__construct(6, 2, true);
+      }
+}
+
+?>
