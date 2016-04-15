1f839a50cc5c9423b76f0876fd58f6d3f5c96de4
redpois0n <red@cmail.nu>
2016-04-15 09:35:34 +0200

    middle prob points

--- awards/middle_probability.php
+++ awards/middle_probability.php
@@ -24,42 +24,42 @@ abstract class MiddleProbability extends AbstractLevelAward {
 class MiddleProbability1 extends MiddleProbability {
 
       public function __construct() {
-            parent::__construct(1, 0.001);
+            parent::__construct(1, 0.001, 1);
       }
 }
 
 class MiddleProbability2 extends MiddleProbability {
 
       public function __construct() {
-            parent::__construct(2, 0.01);
+            parent::__construct(2, 0.01, 2);
       }
 }
 
 class MiddleProbability3 extends MiddleProbability {
 
       public function __construct() {
-            parent::__construct(3, 0.1);
+            parent::__construct(3, 0.1, 5);
       }
 }
 
 class MiddleProbability4 extends MiddleProbability {
 
       public function __construct() {
-            parent::__construct(4, 0.5);
+            parent::__construct(4, 0.5, 10);
       }
 }
 
 class MiddleProbability5 extends MiddleProbability {
 
       public function __construct() {
-            parent::__construct(5, 1);
+            parent::__construct(5, 1, 20);
       }
 }
 
 class MiddleProbability6 extends MiddleProbability {
 
       public function __construct() {
-            parent::__construct(6, 2, true);
+            parent::__construct(6, 2, 30, true);
       }
 }
 
