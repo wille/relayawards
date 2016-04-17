4e9081a27731b1d34d9de6dc1b870535415f8b4d
redpois0n <red@cmail.nu>
2016-04-17 10:19:06 +0200

    red contact info added

--- contact.php
+++ contact.php
@@ -3,14 +3,22 @@ $title = "Contact - RelayAwards";
 
 require_once "header.php";
 ?>
-<head>
-<meta charset="UTF-8">
-<meta name="keywords" content="contact,relayawards,intochloe,redpois0n,github,tor,relay,award,awards,questions,email">
-<meta name="description" content="RelayAwards - A competition for Tor relay operators! - contact the creators. dotchloe and redpois0n">
-</head>
-<center><h2>Contact</h2></br>
-<img src="images/static/chloe.png">
-</br></br>
-<i class="fa fa-envelope" aria-hidden="true"></i> chloe@chloe.re </br>
-<i class="fa fa-twitter" aria-hidden="true"></i> <a href="https://twitter.com/dotchloe"> @dotchloe </a></br>
-<i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/intchloe"> intchloe </a>
+            <center>
+                  <h2>Contact</h2>
+                  </br>
+                  <img src="images/static/chloe.png">
+                  </br></br>
+
+                  <i class="fa fa-envelope" aria-hidden="true"></i> chloe@chloe.re </br>
+                  <i class="fa fa-twitter" aria-hidden="true"></i> <a href="https://twitter.com/dotchloe"> @dotchloe </a></br>
+                  <i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/intchloe"> intchloe </a>
+
+                  </br>
+                  <a href="https://redpois0n.com"><img src="images/static/red.png" width=150px height=150px></a>
+                  </br></br>
+
+                  <i class="fa fa-envelope" aria-hidden="true"></i> red@cmail.nu </br>
+                  <i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/redpois0n"> redpois0n </a>
+            </center>
+      </body>
+</html>
