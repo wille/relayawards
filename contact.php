5c9d8ca437ede8a00f28db73cf68921798e57c86
intchloe <chloe@cmail.nu>
2016-04-18 17:35:01 +0200

    Added more spacing

--- contact.php
+++ contact.php
@@ -8,15 +8,11 @@ require_once "header.php";
                   </br>
                   <img src="images/static/chloe.png">
                   </br></br>
-
                   <i class="fa fa-envelope" aria-hidden="true"></i> chloe@chloe.re </br>
                   <i class="fa fa-twitter" aria-hidden="true"></i> <a href="https://twitter.com/dotchloe"> @dotchloe </a></br>
                   <i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/intchloe"> intchloe </a>
-
-                  </br>
+                  </br></br></br>
                   <a href="https://redpois0n.com"><img src="images/static/red.png" width=150px height=150px></a>
-                  </br></br>
-
                   <i class="fa fa-envelope" aria-hidden="true"></i> red@cmail.nu </br>
                   <i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/redpois0n"> redpois0n </a>
             </center>
