34ece4ac320d24c703df4965d46b311036d9de3f
redpois0n <red@cmail.nu>
2016-04-18 15:22:32 +0200

    status messages

--- cache/generate_top.php
+++ cache/generate_top.php
@@ -6,12 +6,16 @@ const FILE = "top_cache.txt";
 
 require_once "cache.php";
 
+echo "Downloading all relays...\n";
 $relays = Relays::query_relays("", false);
 
+echo "Sorting relays (0 -> " . AMOUNT . ")\n";
 $relays = sort_relays($relays, 0, AMOUNT);
 
+echo "Generating cache...\n";
 $cached = cache_multiple($relays);
 
+echo "Writing to " . FILE . "\n";
 file_put_contents(FILE, json_encode($cached));
 
 ?>
