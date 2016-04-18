acbcd2f01a131b7a8f1aa4017e1e1fc05b9c0d2d
redpois0n <red@cmail.nu>
2016-04-18 15:33:07 +0200

    Cache generator for all relays added

---
+++ cache/generate_all.php
@@ -0,0 +1,19 @@
+<?php
+
+const FILE = "cache.txt";
+
+require_once "cache.php";
+
+echo "Downloading all relays...\n";
+$relays = Relays::query_relays("", false);
+
+echo "Sorting relays (0 -> " . count($relays) . ")\n";
+$relays = sort_relays($relays);
+
+echo "Generating cache...\n";
+$cached = cache_multiple($relays);
+
+echo "Writing to " . FILE . "\n";
+file_put_contents(FILE, json_encode($cached));
+
+?>
