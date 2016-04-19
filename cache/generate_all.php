785ad8dc71d517b213d438576ce6343a750e010a
redpois0n <red@cmail.nu>
2016-04-19 18:30:22 +0200

    generate_all now runs all generate scripts

--- cache/generate_all.php
+++ cache/generate_all.php
@@ -1,23 +1,11 @@
 <?php
 
-const FILE = "cache.txt";
+echo "Generating index...\n";
+include "generate_index.php";
 
-require_once "cache.php";
+echo "\n";
 
-echo "Downloading all relays...\n";
-$relays = Relays::query_relays("");
-
-if (count($relays) == 0) {
-      die("No relays found, will not overwrite existing cache");
-}
-
-echo "Sorting relays (0 -> " . count($relays) . ")\n";
-$relays = sort_relays($relays);
-
-echo "Generating cache...\n";
-$cached = cache_multiple($relays);
-
-echo "Writing to " . FILE . "\n";
-file_put_contents(FILE, json_encode($cached));
+echo "Generating top list\n";
+include "generate_top.php";
 
 ?>
---
+++ cache/generate_index.php
@@ -0,0 +1,23 @@
+<?php
+
+const FILE = "cache.txt";
+
+require_once "cache.php";
+
+echo "Downloading all relays...\n";
+$relays = Relays::query_relays("");
+
+if (count($relays) == 0) {
+      die("No relays found, will not overwrite existing cache");
+}
+
+echo "Sorting relays (0 -> " . count($relays) . ")\n";
+$relays = sort_relays($relays);
+
+echo "Generating cache...\n";
+$cached = cache_multiple($relays);
+
+echo "Writing to " . FILE . "\n";
+file_put_contents(FILE, json_encode($cached));
+
+?>
