ac1615dd8d40c973c74339f4c7e490487de4fc49
redpois0n <red@cmail.nu>
2016-08-25 15:07:59 +0100

    Search by continent

--- index.php
+++ index.php
@@ -49,12 +49,30 @@ function match($relay, $search) {
             $relay["country_name"],
       ];
 
+      $continents = [
+             "Europe" => ["AL","AD","AT","BY","BE","BA","BG","HR","CY","CZ","DK","EE","FO","FI","FR","DE","GI","GR","HU","IS","IE","IT","LV","LI","LT","LU","MK","MT","MD","MC","NL","NO","PL","PT","RO","RU","SM","RS","SK","SI","ES","SE","CH","UA","GB","VA","RS","IM","RS","ME"],
+             "Africa" => ["DZ","AO","SH","BJ","BW","BF","BI","CM","CV","CF","TD","KM","CG","DJ","EG","GQ","ER","ET","GA","GM","GH","GW","GN","CI","KE","LS","LR","LY","MG","MW","ML","MR","MU","YT","MA","MZ","NA","NE","NG","ST","RE","RW","ST","SN","SC","SL","SO","ZA","SH","SD","SZ","TZ","TG","TN","UG","CD","ZM","TZ","ZW","SS","CD"],
+             "Asia" => ["AF","AM","AZ","BH","BD","BT","BN","KH","CN","CX","CC","IO","GE","HK","IN","ID","IR","IQ","IL","JP","JO","KZ","KP","KR","KW","KG","LA","LB","MO","MY","MV","MN","MM","NP","OM","PK","PH","QA","SA","SG","LK","SY","TW","TJ","TH","TR","TM","AE","UZ","VN","YE","PS"],
+             "North America" => ["AI","AG","AW","BS","BB","BZ","BM","VG","CA","KY","CR","CU","CW","DM","DO","SV","GL","GD","GP","GT","HT","HN","JM","MQ","MX","PM","MS","CW","KN","NI","PA","PR","KN","LC","PM","VC","TT","TC","VI","US","SX","BQ","SA"],
+             "South America" => ["AR","BO","BR","CL","CO","EC","FK","GF","GY","GY","PY","PE","SR","UY","VE"]
+      ];
+
       $search_array = explode("+", $search);
 
       foreach ($search_array as $s) {
             foreach ($to_search as $term) {
                   if (strpos(strtolower($term), strtolower($s)) !== false) {
                         return true;
+                  }
+            }
+      }
+
+      foreach ($continents as $continent) {
+            if (strpos(strtolower($continent), strtolower($search)) !== false) {
+                  foreach ($continent as $country) {
+                        if ($relay["country"] == strtoupper($country)) {
+                              return true;
+                        }
                   }
             }
       }
